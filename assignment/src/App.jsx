75fca49a57bc28382b04a9bc62ef481c7cc39162
Ruhannn <kamiayaka09@gmail.com>
2023-09-14 21:54:21 +0600

    react + taiwind css setup

---
+++ assignment/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <h1 className="text-center">Course Registration</h1>
+      {/* container */}
+      <div className="container">
+        {/* Course */}
+        <div>
+
+        </div>
+        {/* total container */}
+        <div></div>
+      </div>
+    </>
+  );
+}
+export default App;
