b0f7dc31c02a36693fee4b943be175b529ba768e
Ruhannn <kamiayaka09@gmail.com>
2023-09-15 08:22:53 +0600

    toast added

--- assignment/src/App.jsx
+++ assignment/src/App.jsx
@@ -1,68 +1,107 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import TotalContainer from "./TotalContainer";
+import toast from "react-hot-toast";
 function CourseList({ courses, selectCourse }) {
-    return (
-      <div className="grid grid-cols-3 gap-7 place-items-center">
-        {courses.map((course, index) => (
-          <div
-            key={index}
-            className="bg-white rounded-xl p-3 grid place-items-center w-80">
-            <img
-              src={course.image}
-              className="mb-4"
-              alt={`Course Image for ${course.name}`}
-            />
-            <h1 className="text-lg font-semibold mb-3 text-center">
-              {course.name}
-            </h1>
-            <p className="text-gray-600 text-sm leading-6 text-center">
-              {course.description}
-            </p>
-            <div className="grid gap-4 my-2 grid-cols-1 sm:grid-cols-2">
-              <div className="flex items-center">
-                <img src="src/assets/dollar.svg" alt="Price Icon" />
-                <h1 className="text-sm">Price: {course.price}</h1>
-              </div>
-              <div className="flex items-center">
-                <img src="src/assets/Frame.svg" alt="Credit Icon" />
-                <h1 className="text-sm">Credit: {course.credit}hr</h1>
-              </div>
+  return (
+    <div className="grid grid-cols-3 gap-7 place-items-center">
+      {courses.map((course, index) => (
+        <div
+          key={index}
+          className="bg-white rounded-xl p-3 grid place-items-center w-80">
+          <img
+            src={course.image}
+            className="mb-4"
+            alt={`Course Image for ${course.name}`}
+          />
+          <h1 className="text-lg font-semibold mb-3 text-center">
+            {course.name}
+          </h1>
+          <p className="text-gray-600 text-sm leading-6 text-center">
+            {course.description}
+          </p>
+          <div className="grid gap-4 my-2 grid-cols-1 sm:grid-cols-2">
+            <div className="flex items-center">
+              <img src="src/assets/dollar.svg" alt="Price Icon" />
+              <h1 className="text-sm">Price: {course.price}</h1>
             </div>
-            <button
-              onClick={() => selectCourse(course)}
-              className="text-white text-lg bg-[#2F80ED] px-28 py-2 mt-2 rounded-md transition duration-300 ease-in-out transform hover:bg-[#4391fa] active:bg-[#4391fa] active:scale-105">
-              Select
-            </button>
+            <div className="flex items-center">
+              <img src="src/assets/Frame.svg" alt="Credit Icon" />
+              <h1 className="text-sm">Credit: {course.credit}hr</h1>
+            </div>
           </div>
-        ))}
-      </div>
-    );
-  }
-  
-  function App() {
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await fetch("/data.json");
-          const data = await response.json();
-          setCourses(data.courses);
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
-      };
-  
-      fetchData();
-    }, []);
-  
-  
-    return (
-      <>
-        <h1 className="text-center text-3xl font-bold my-8">
-          Course Registration
-        </h1>
+          <button
+            onClick={() => selectCourse(course)}
+            className="text-white text-lg bg-[#2F80ED] px-28 py-2 mt-2 rounded-md transition duration-300 ease-in-out transform hover:bg-[#4391fa] active:bg-[#4391fa] active:scale-105">
+            Select
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
 
-      </>
+function App() {
+  const [courses, setCourses] = useState([]);
+  const maxCredits = 20;
+  const [remainingCredits, setRemainingCredits] = useState(maxCredits);
+  const [selectedCourses, setSelectedCourses] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/data.json");
+        const data = await response.json();
+        setCourses(data.courses);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const selectCourse = (course) => {
+    const isAlreadySelected = selectedCourses.some(
+      (selectedCourse) => selectedCourse.name === course.name
     );
-  }
+
+    if (isAlreadySelected) {
+      toast.error(`You've already selected ${course.name}.`);
+      // alert(`You've already selected ${course.name}.`);
+    } else if (remainingCredits - course.credit >= 0) {
+      setRemainingCredits(remainingCredits - course.credit);
+      setSelectedCourses([...selectedCourses, course]);
+    } else {
+      toast.error("Not enough credits remaining to select this course.");
+    }
+  };
+  const totalCreditHours = selectedCourses.reduce(
+    (total, course) => total + course.credit,
+    0
+  );
+  const totalPrice = selectedCourses.reduce(
+    (total, course) => total + course.price,
+    0
+  );
+
+  return (
+    <>
+      <h1 className="text-center text-3xl font-bold my-8">
+        Course Registration
+      </h1>
+      <section className="container mx-auto px-14 mt-12 flex gap-5">
+        {/* Course */}
+        <CourseList courses={courses} selectCourse={selectCourse} />
+        {/* Total container */}
+        <TotalContainer
+          remainingCredits={remainingCredits}
+          totalCreditHours={totalCreditHours}
+          totalPrice={totalPrice}
+          selectedCourses={selectedCourses}
+        />
+      </section>
+    </>
+  );
+}
 export default App;
--- assignment/src/TotalContainer.jsx
+++ assignment/src/TotalContainer.jsx
@@ -1,3 +1,27 @@
 import React from "react";
-
+function TotalContainer({ remainingCredits, totalCreditHours, totalPrice, selectedCourses }) {
+    return (
+      <div className="bg-white rounded-lg p-6 h-[380px]">
+        <h1 className="text-[#2F80ED] text-lg font-bold leading-8 mb-4 pb-3 border-b-2 border-[#1c1b1b] border-opacity-20">
+          Credit Hour Remaining {remainingCredits} hr
+        </h1>
+        <h1 className="text-[#1C1B1B] text-xl font-bold leading-normal">
+          Course Name
+        </h1>
+        <ul className="text-[#1C1B1B] leading-normal list-decimal">
+          {selectedCourses.map((course, index) => (
+            <li key={index} className="ml-5 leading-8">
+              {course.name}
+            </li>
+          ))}
+        </ul>
+        <h1 className="text-[#1c1b1b] py-4 my-4 text-opacity-80 border-b-2 border-t-2 border-[#1c1b1b] border-opacity-20">
+          Total Credit Hour : {totalCreditHours}
+        </h1>
+        <h1 className="text-[#141313] font-semibold mb-3">
+          Total Price : {totalPrice} USD
+        </h1>
+      </div>
+    );
+  }
 export default TotalContainer;
